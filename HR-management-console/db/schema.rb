dd99bb70912c1e6b99ca1bf65756640355adca23
veda <arvindveda3@gmail.com>
2017-06-15 11:24:44 +0530

    asfs

--- HR-management-console/db/schema.rb
+++ HR-management-console/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614110154) do
+ActiveRecord::Schema.define(version: 20170614102450) do
 
   create_table "employee_records", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -28,9 +28,6 @@ ActiveRecord::Schema.define(version: 20170614110154) do
   create_table "records", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "employee_name"
-    t.string "project"
-    t.string "team_lead"
   end
 
   create_table "skills", force: :cascade do |t|
